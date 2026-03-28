65703f719878c8ca61e42fe8b8fc0aeac32ad323
Louise sKinner <louise.skinner40@gmail.com>
2026-03-28 20:35:55 +0000

    Added product detail functionailty

--- products/urls.py
+++ products/urls.py
@@ -5,4 +5,5 @@ app_name = 'products'
 
 urlpatterns = [
     path('', views.all_products, name='products'),
+    path('<product_id>', views.product_details, name='product_details'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
-# Create your views here.
+
 def all_products(request):
+    """ A view to show all products """
     # Retrieves all products from the database
     products = Product.objects.all()
 
@@ -13,3 +14,15 @@ def all_products(request):
 
     # Loads the HTML template
     return render(request, 'products/products.html', context)
+
+def product_details(request, product_id):
+    """ A view to show individual proect details """
+    
+    product = get_object_or_404(Product, pk=product_id)
+    # Sends the products to the template      
+    context = {
+        'product': product,
+    }
+
+    # Loads the HTML template
+    return render(request, 'products/product_details.html', context)

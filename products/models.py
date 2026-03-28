f3eec6f81f4b71823f95219ce0966a628ea26bf0
Louise sKinner <louise.skinner40@gmail.com>
2026-03-28 18:17:52 +0000

    Added products and categories json files and updated the products models

--- products/models.py
+++ products/models.py
@@ -2,11 +2,19 @@ from django.db import models
 
 # Create your models here.
 
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=254)
+
+    def __str__(self):
+         return self.name
+
 class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    category = models.CharField(max_length=100)
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     image = models.ImageField(upload_to='products/', blank=True)
 
     def __str__(self):

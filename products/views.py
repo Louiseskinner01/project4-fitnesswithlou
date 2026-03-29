2c4dfde3c8b4367afcc100172d151c97bf7c1ce0
Louise sKinner <louise.skinner40@gmail.com>
2026-03-29 09:29:39 +0100

    Added query function so users can seach for products in the search bar

--- products/views.py
+++ products/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Product
 
 
@@ -6,14 +8,28 @@ def all_products(request):
     """ A view to show all products """
     # Retrieves all products from the database
     products = Product.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('products:products'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
 
     # Sends the products to the template      
     context = {
         'products': products,
+        'search_term': query,
     }
 
     # Loads the HTML template
     return render(request, 'products/products.html', context)
+
+
 
 def product_details(request, product_id):
     """ A view to show individual proect details """
@@ -22,6 +38,7 @@ def product_details(request, product_id):
     # Sends the products to the template      
     context = {
         'product': product,
+        
     }
 
     # Loads the HTML template

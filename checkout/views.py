c711f27f894b21fedfc44ca26226e603a088a445
Louise sKinner <louise.skinner40@gmail.com>
2026-04-02 21:07:37 +0100

    Removed Stripe keys from code

--- checkout/views.py
+++ checkout/views.py
@@ -3,6 +3,10 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from cart.models import Cart
 from .forms import OrderForm
+import stripe
+from django.conf import settings
+
+stripe.api_key = settings.STRIPE_SECRET_KEY
 
 @login_required
 def checkout(request):
@@ -21,6 +25,7 @@ def checkout(request):
         'order_form': order_form,
         'cart_items': cart_items,
         'total': total,
-    }
+        'stripe_public_key': settings.STRIPE_PUBLIC_KEY,    
+        }
 
     return render(request, 'checkout/checkout.html', context)
--- fwl/settings.py
+++ fwl/settings.py
@@ -1,6 +1,8 @@
 import os
 from pathlib import Path
 
+STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY')
+STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent

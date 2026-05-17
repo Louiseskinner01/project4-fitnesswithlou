88c9d4b0bf497d9ea4967905e77f55a9b8e65701
Louise sKinner <louise.skinner40@gmail.com>
2026-05-17 21:37:30 +0100

    Added Crispy Forms to allauth pages, styled inputs and removed labels

---
+++ accounts/forms.py
@@ -0,0 +1,22 @@
+from allauth.account.forms import LoginForm
+from crispy_forms.helper import FormHelper
+
+
+class CustomLoginForm(LoginForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # Hide labels
+        self.fields['login'].label = False
+        self.fields['password'].label = False
+
+        # Add placeholders
+        self.fields['login'].widget.attrs['placeholder'] = 'Email Address'
+        self.fields['password'].widget.attrs['placeholder'] = 'Password'
+
+        # Add classes
+        self.fields['login'].widget.attrs['class'] = 'form-control auth-input'
+        self.fields['password'].widget.attrs['class'] = 'form-control auth-input'
+
+        self.helper = FormHelper()
--- fwl/settings.py
+++ fwl/settings.py
@@ -116,6 +116,8 @@ LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
+ACCOUNT_FORMS = {'login': 'accounts.forms.CustomLoginForm',}
+
 
 
 

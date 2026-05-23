8383230fb05edfd3b5b20a951416faf9263b4b80
Louise sKinner <louise.skinner40@gmail.com>
2026-05-23 22:11:54 +0100

    Upload fields and style product management page

--- products/forms.py
+++ products/forms.py
@@ -1,18 +1,83 @@
 from django import forms
 from .models import Product, Category
 
+
+SIZE_CHOICES = (
+    ('xs', 'XS'),
+    ('s', 'S'),
+    ('m', 'M'),
+    ('l', 'L'),
+    ('xl', 'XL'),
+)
+
+
 class ProductForm(forms.ModelForm):
+
+    sizes = forms.ChoiceField(
+        choices=[('', 'Select Size')] + list(SIZE_CHOICES),
+        required=False,
+        widget=forms.Select(attrs={'class': 'size-select'})
+    )
 
     class Meta:
         model = Product
         fields = '__all__'
-    
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        Categories = Category.objects.all()
-        friendly_names = [(c.id, c.get_friendly_name())
-                            for c in Categories
-                        ]
-        self.fields['category'].choices = friendly_names
+
+        categories = Category.objects.all()
+
+        friendly_names = [
+            (c.id, c.get_friendly_name())
+            for c in categories
+        ]
+
+        self.fields['category'].choices = [
+            ('', 'Select Category')
+        ] + friendly_names
+
+        placeholders = {
+            'name': 'Product Name',
+            'description': 'Product Description',
+            'price': 'Price',
+            'image_url': 'Image URL',
+        }
+
         for field_name, field in self.fields.items():
-            field.widget.attrs['class'] = 'border-black rounded-0'
+
+            field.label = False
+            field.widget.attrs['class'] = 'rounded'
+
+            if field_name in placeholders and field.widget.input_type in ['text', 'number', 'textarea', 'email']:
+                field.widget.attrs['placeholder'] = placeholders[field_name]
+
+    def save(self, commit=True):
+        instance = super().save(commit=False)
+
+        if self.cleaned_data.get('sizes'):
+            instance.sizes = self.cleaned_data['sizes']
+
+        if commit:
+            instance.save()
+
+        return instance
+
+    sizes = forms.ChoiceField(
+        choices=[('', 'Select Sizes')] + list(SIZE_CHOICES),
+        required=False
+    )
+
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+    def save(self, commit=True):
+        instance = super().save(commit=False)
+
+        instance.sizes = ",".join(self.cleaned_data['sizes'])
+
+        if commit:
+            instance.save()
+
+        return instance
--- products/models.py
+++ products/models.py
@@ -5,6 +5,9 @@ from django.db import models
 
 
 class Category(models.Model):
+    class Meta:
+        verbose_name_plural = "Categories"  
+
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
@@ -16,13 +19,9 @@ class Category(models.Model):
 
 class Product(models.Model):
 
-    class Meta:
-        verbose_name = "Category"
-        verbose_name_plural = "Categories"  # correct plural
-
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=100)
-    has_sizes = models.BooleanField(default=False, null=True, blank=True)
+    sizes = models.CharField(max_length=50, blank=True, help_text="Comma separated sizes e.g. s,m,l,xl")   
     price = models.DecimalField(max_digits=6, decimal_places=2)
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     image = models.ImageField(upload_to='products/', blank=True)

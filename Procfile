fca6599496d569a63cdd2e3f80fe2f93d5bbb4cc
Louise sKinner <louise.skinner40@gmail.com>
2026-06-12 21:56:35 +0100

    Add Procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn fwl.wsgi:application
--- fwl/settings.py
+++ fwl/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--wwc4!o8$lapuxv!@l*(isu_s&=jd#y7zanwc%ag6fr0*8qkjh'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'jango-insecure--wwc4!o8$lapuxv!@l*(isu_s&=jd#y7zanwc%ag6fr0*8qkjh')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

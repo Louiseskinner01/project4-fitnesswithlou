4e4e36fd25624623356cab53f5860b8a628897bd
Louise sKinner <louise.skinner40@gmail.com>
2026-06-14 14:32:46 +0100

    Fix whitenoise config

--- fwl/settings.py
+++ fwl/settings.py
@@ -196,7 +196,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')

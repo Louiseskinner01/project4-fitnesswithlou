465661c4dfffbab74e947563d0b625fe47f372a4
Louise sKinner <louise.skinner40@gmail.com>
2026-06-24 22:48:55 +0100

    Remove try except from cart content

--- cart/contexts.py
+++ cart/contexts.py
@@ -8,40 +8,33 @@ def cart_contents(request):
     total = 0
     product_count = 0
     cart = None
+    
+    if request.user.is_authenticated:
+        cart = Cart.objects.filter(user=request.user).first()
 
-    try:
-        if request.user.is_authenticated:
-            cart = Cart.objects.filter(user=request.user).first()
+    if cart:
+        for item in cart.items.all():
+            product = item.product
+            quantity = item.quantity
+            total += product.price * quantity
+            product_count += quantity
+            cart_items.append({
+                'id': item.id,
+                'product': item.product,
+                'quantity': item.quantity,
+                'price': product.price,
+                'size': item.size,
+            })
 
-        if cart:
-            for item in cart.items.all():
-                product = item.product
-                quantity = item.quantity
-
-                total += product.price * quantity
-                product_count += quantity
-
-                cart_items.append({
-                    'id': item.id,
-                    'product': item.product,
-                    'quantity': item.quantity,
-                    'price': product.price,
-                    'size': item.size,
-                })
-
-        if total < settings.FREE_DELIVERY_ABOVE:
-            delivery = total * Decimal(settings.DELIVERY_PERCENTAGE / 100)
-            free_delivery_delta = settings.FREE_DELIVERY_ABOVE - total
-        else:
-            delivery = 0
-            free_delivery_delta = 0
-
-    except Exception:
+    if total < settings.FREE_DELIVERY_ABOVE:
+        delivery = total * Decimal(settings.DELIVERY_PERCENTAGE / 100)
+        free_delivery_delta = settings.FREE_DELIVERY_ABOVE - total
+    else:
         delivery = 0
         free_delivery_delta = 0
-
+    
     grand_total = delivery + total
-
+    
     context = {
         'cart_items': cart_items,
         'total': total,
